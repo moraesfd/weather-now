c971115a68902ff90fae61e580510271efbbb4c0
Felipe de Moraes <moraesfd@gmail.com>
2021-12-24 18:14:15 -0300

    feat: api service file and show data

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueMeta from 'vue-meta'
-import '@/assets/styles/reset.css';
+import '@/assets/styles/reset.css'
 
 Vue.use(VueMeta)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: (h) => h(App)
+}).$mount('#app')
